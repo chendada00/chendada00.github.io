a7176f78b11b9efbafe4aa54f382a1948293f28b
陈波 <2207117672@qq.com>
2024-02-25 18:13:17 +0800

    Site updated: 2024-02-25 18:13:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/24/arthas用法/","2024/02/23/hello-world/","2024/02/25/window查看端口占用/"];function toRandomPost(){
+var posts=["2024/02/24/arthas用法/","2024/02/25/window查看端口占用/","2024/02/23/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
