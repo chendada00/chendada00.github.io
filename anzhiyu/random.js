4009298f9c8b51c8b6ee1f7fb1e7c7655b5f91a8
陈波 <2207117672@qq.com>
2024-02-27 22:01:50 +0800

    Site updated: 2024-02-27 22:01:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/25/window查看端口占用/","2024/02/23/hello-world/","2024/02/24/arthas用法/"];function toRandomPost(){
+var posts=["2024/02/24/arthas用法/","2024/02/23/hello-world/","2024/02/26/软考考点总结/","2024/02/25/window查看端口占用/","2024/02/27/java内存分析命令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
