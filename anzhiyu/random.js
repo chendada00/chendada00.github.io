2a4459c0de07b505d9085acae83ecb3104af1ae0
陈波 <2207117672@qq.com>
2024-03-16 12:11:02 +0800

    Site updated: 2024-03-16 12:10:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/24/arthas用法/","2024/02/25/window查看端口占用/","2024/02/27/java内存分析命令/","2024/02/26/《软件设计师》考点总结/","2024/03/12/前端常用CDN查询/","2024/03/15/改造你的微信传输助手/"];function toRandomPost(){
+var posts=["2024/02/24/arthas用法/","2024/02/25/window查看端口占用/","2024/02/27/java内存分析命令/","2024/02/26/《软件设计师》考点总结/","2024/03/12/前端常用CDN查询/","2024/03/15/改造你的微信传输助手/","2024/03/16/hexo博客源码自动备份/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
